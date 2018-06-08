bae1728491defadbd797b445ecc2f0f9e14376f2
JR Bye <jrbye@HDS902552.local>
2018-06-08 16:06:10 -0500

    Step3a

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -6,8 +8,29 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
-            //GradeBookType gradeBookType = new GradeBookType();
             Type = GradeBookType.Ranked;
+        }     
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("5 Students Required for Ranked Grade Book");
+            }
+            
+            var ceiling = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[ceiling - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(ceiling*2)-1]<= averageGrade)
+                return 'B';
+            else if (grades[(ceiling*3)-1]<= averageGrade)
+                return 'C';
+            else if (grades[(ceiling*4)-1]<= averageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
